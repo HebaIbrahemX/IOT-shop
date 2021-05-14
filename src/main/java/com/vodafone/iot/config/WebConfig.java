72b307433c4234b6a5f5dd4329bb5502e6073c3c
Heba Ibrahem <heba@latefangers.zte.com.cn>
2021-05-15 00:54:23 +0200

    web config for integration test

---
+++ src/main/java/com/vodafone/iot/config/WebConfig.java
@@ -0,0 +1,14 @@
+package com.vodafone.iot.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan("com.vodafone.iot.*")
+public class WebConfig implements WebMvcConfigurer {
+    
+}
+

387f1334d082b1e32011110aea03c34b36df0c0a
Heba Ibrahem <heba@latefangers.zte.com.cn>
2021-05-14 22:00:15 +0200

    device status mapping

---
+++ src/main/java/com/vodafone/iot/utils/DeviceStatus.java
@@ -0,0 +1,9 @@
+package com.vodafone.iot.utils;
+
+public class DeviceStatus {
+    public static final int ACTIVE =1;
+    public static final int WAITING_FOR_ACTIVATION =2;
+    public static final int BLOCKED =3;
+    public static final int DEACTIVATED =4;
+    
+}

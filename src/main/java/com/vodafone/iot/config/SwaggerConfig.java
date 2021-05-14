afe493263dca36b484ab131b2bc6fc07a350382c
Heba Ibrahem <heba@latefangers.zte.com.cn>
2021-05-15 01:00:21 +0200

    removed extra http

--- src/main/java/com/vodafone/iot/config/SwaggerConfig.java
+++ src/main/java/com/vodafone/iot/config/SwaggerConfig.java
@@ -22,7 +22,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig implements WebMvcConfigurer {
-    String  baseUrl="http://localhost:8080";
+    String  baseUrl="localhost:8080";
 
 
   @Override

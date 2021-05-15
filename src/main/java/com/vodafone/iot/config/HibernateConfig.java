3c712f09a2f03e8b49f7b7023000d7c92711da0c
Heba Ibrahem <heba@latefangers.zte.com.cn>
2021-05-15 02:11:25 +0200

    changing db config to accept cli args

--- src/main/java/com/vodafone/iot/config/HibernateConfig.java
+++ src/main/java/com/vodafone/iot/config/HibernateConfig.java
@@ -5,17 +5,26 @@ import com.vodafone.iot.model.Device;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.PropertySource;
 
+@PropertySource("classpath:application.properties")
 @org.springframework.context.annotation.Configuration 
 public class HibernateConfig {
 
     private SessionFactory factorysession;
+    @Value("${db.user}")
+    private String dbUser;
+    @Value("${db.pass}")
+    private String dbPass;
    
     @Bean
     public SessionFactory getSessionFactory() {
         Configuration cfg = new Configuration();
         cfg.configure("hibernate.cfg.xml");
+        cfg.setProperty("hibernate.connection.username",dbUser );
+        cfg.setProperty("hibernate.connection.password",dbPass );
         cfg.addAnnotatedClass(Device.class);
         
         try {

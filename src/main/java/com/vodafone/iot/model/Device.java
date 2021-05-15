440cbe59f3f278980aa7cb12d8199933fec0333f
Heba Ibrahem <heba@latefangers.zte.com.cn>
2021-05-15 03:10:13 +0200

    removing lombok

--- src/main/java/com/vodafone/iot/model/Device.java
+++ src/main/java/com/vodafone/iot/model/Device.java
@@ -7,11 +7,9 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
-import lombok.Getter;
-import lombok.Setter;
 
-@Setter
-@Getter
+
+
 @Entity
 @Table(name="iot_device")
 public class Device {

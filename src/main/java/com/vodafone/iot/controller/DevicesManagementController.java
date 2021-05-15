87ced02f7a19d372b35d9cb7da0d6231a039fc97
Heba Ibrahem <heba@latefangers.zte.com.cn>
2021-05-15 05:40:01 +0200

    adding custom example value for swagger

--- src/main/java/com/vodafone/iot/controller/DevicesManagementController.java
+++ src/main/java/com/vodafone/iot/controller/DevicesManagementController.java
@@ -3,9 +3,7 @@ package com.vodafone.iot.controller;
 import com.vodafone.iot.dto.DeviceDTO;
 import com.vodafone.iot.services.DevicesService;
 
-import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,6 +11,9 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
 
 @RestController
 @RequestMapping(path = "/iot-shop/admin")
@@ -23,6 +24,7 @@ public class DevicesManagementController {
     
 
     @PutMapping(produces = "application/json")
+    @ApiImplicitParams({@ApiImplicitParam(paramType = "body" ,name="object",example="{\"status\":1,\"simId\":\"5#23\"}",dataTypeClass=String.class )})
     public ResponseEntity<Object>updateDevice(@RequestBody DeviceDTO object){
 
         return service.updateDevice(object);
